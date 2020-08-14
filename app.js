322a7250bfa5e5b6d908ca19e7ef570f7dcbdb54
Utsav Goswami <utsavg19@googlemail.com>
2020-08-14 12:27:22 -0700

    First Version

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded(
+    { extended: true }
+));
+
+app.use(express.static("public"));
+
 app.set('view engine', 'ejs');
+
+const items = ["Buy Food", "Eat Food", "Cook Food"];
+
+// Handle user form input
+app.post("/", (req, res) => {
+    const item = req.body.taskName;
+
+    items.push(item);
+
+    res.redirect("/");
+})
 
 app.get("/", (req, res) => {
     const today = new Date();
@@ -32,9 +49,12 @@ app.get("/", (req, res) => {
     const day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItem: items
     });
 });
+
+
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
